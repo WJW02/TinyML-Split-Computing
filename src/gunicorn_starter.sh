90c858666307983f3f3b47bff63f6fd56d38ed33
Fabio Bove <fabio.bove.dr@gmail.com>
2024-06-27 17:54:27 +0200

    FIX: shell scripts warnings

--- src/gunicorn_starter.sh
+++ src/gunicorn_starter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gunicorn_workers="${gunicorn_workers:-1}"
+gunicornWorkers="${gunicorn_workers:-1}"
 echo "gunicorn_workers: ${gunicorn_workers}"
-gunicorn --log-level debug --keep-alive 5 --workers ${gunicorn_workers} --b 0.0.0.0:8000 api.wsgi --timeout 120 app:app
+gunicorn --log-level debug --keep-alive 5 --bind 0.0.0.0:8080 --workers="$gunicornWorkers" --timeout 120 app:app
