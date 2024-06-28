56359215e3f75a87c386e3070488a00fbfdf7047
Fabio Bove <fabio.bove.dr@gmail.com>
2024-06-28 12:43:06 +0200

    REFACTOR: renamed class modules files

--- src/flask_server/offloading/offloading_algo.py
+++ src/flask_server/offloading/offloading_algo.py
@@ -1,11 +1,16 @@
-from flask_server.logger.Logger import Logger
+from logger.Logger import Logger
 
-logger = Logger(config_path='logger_config.json').get_logger(__name__)
+logger = Logger().get_logger(__name__)
 
 
 class OffloadingAlgo:
-    def __init__(self, avg_speed: float, num_layers: int, layers_sizes: list, inference_time_device: list,
-                 inference_time_edge: list) -> None:
+    def __init__(self,
+                 avg_speed: float,
+                 num_layers: int,
+                 layers_sizes: list,
+                 inference_time_device: list,
+                 inference_time_edge: list
+                 ) -> None:
         self.avg_speed = avg_speed
         self.num_layers = num_layers
         self.layers_sizes = layers_sizes
@@ -16,13 +21,13 @@ class OffloadingAlgo:
         self.lowest_evaluation = float('inf')
 
     @staticmethod
-    def evaluation(initial_cost: int, layer_data_size: float, edge_computation_costs: list, avg_speed: float):
+    def evaluation(initial_cost: float, layer_data_size: float, edge_computation_cost: list, avg_speed: float):
         logger.info(f"Performing Evaluation:")
         logger.info(f"DS:           {layer_data_size}")
         logger.info(f"IC:           {initial_cost}")
-        logger.info(f"CE:           {edge_computation_costs}")
+        logger.info(f"CE:           {edge_computation_cost}")
         logger.info(f"AvgSpeed:     {avg_speed}")
-        evaluation = round(initial_cost + (layer_data_size / avg_speed) + edge_computation_costs, 3)
+        evaluation = round(initial_cost + (layer_data_size / avg_speed) + edge_computation_cost, 3)
         logger.info(f"Evaluation =  {evaluation}")
         return evaluation
 
@@ -32,13 +37,13 @@ class OffloadingAlgo:
 
         initial_cost = 0
         first_layer_size = self.layers_sizes[0]
-        edge_computation_costs = sum(self.inference_time_edge[:self.num_layers + 1])
+        edge_computation_cost = sum(self.inference_time_edge[:self.num_layers + 1])
 
         self.lowest_evaluation = self.evaluation(
             initial_cost=initial_cost,
             layer_data_size=first_layer_size,
             avg_speed=self.avg_speed,
-            edge_computation_costs=edge_computation_costs,
+            edge_computation_cost=edge_computation_cost,
         )
 
     def mixed_computation_evaluation(self):
@@ -46,14 +51,14 @@ class OffloadingAlgo:
         logger.info(f"Performing Partial Offloading:")
         for layer in range(0, self.num_layers):
             initial_cost = (0 if layer == 0 else sum(self.inference_time_device[:layer]))
-            edge_computation_costs = sum(self.inference_time_edge[layer:self.num_layers])
+            edge_computation_cost = sum(self.inference_time_edge[layer:self.num_layers])
             layer_data_size = self.layers_sizes[layer + 1]
 
             evaluation = self.evaluation(
                 initial_cost=initial_cost,
                 layer_data_size=layer_data_size,
                 avg_speed=self.avg_speed,
-                edge_computation_costs=edge_computation_costs,
+                edge_computation_cost=edge_computation_cost,
             )
 
             if evaluation < self.lowest_evaluation:
@@ -64,12 +69,13 @@ class OffloadingAlgo:
         logger.info(f"Performing Device Only Offloading:")
         initial_cost = sum(self.inference_time_device[:self.num_layers + 1])
         layer_data_size = 0
+        edge_computation_cost = 0
         # No Offloading: Device Only Computation
         last_evaluation = self.evaluation(
             initial_cost=initial_cost,
             layer_data_size=layer_data_size,
             avg_speed=self.avg_speed,
-            edge_computation_costs=0,
+            edge_computation_cost=edge_computation_cost,
         )
         if last_evaluation < self.lowest_evaluation:
             self.best_offloading_layer = self.num_layers
