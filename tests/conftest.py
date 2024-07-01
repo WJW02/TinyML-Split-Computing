740975bdd50d40a808c00d9fd7d738cc8007ef6c
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-01 23:23:42 +0200

    ADD: Fixtures for message in conftest

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import json
+
+import pytest
+
+from flask_server.offloading.offloading_message import OffloadingMessage
+
+
+class MockResponse:
+    def __init__(self, text):
+        self.text = text
+
+
+@pytest.fixture
+def message():
+    message_data = MockResponse(json.dumps({'key': 'value'}))
+    return OffloadingMessage(message_data=message_data)
