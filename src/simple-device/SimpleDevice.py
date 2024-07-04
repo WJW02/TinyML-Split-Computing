911a53f1caa9022c52f9a08fccc1f16b2fb11ffa
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-04 20:05:17 +0200

    UPDATE: class for integration tests

--- src/simple-device/SimpleDevice.py
+++ src/simple-device/SimpleDevice.py
@@ -11,16 +11,16 @@ class SimpleDevice:
     def __init__(self, device_id: str, message_data: dict):
         self.device_id = device_id
         self.message_data = message_data
+        self.response = None
 
     def send_message(self) -> None:
         self.message_data['device_id'] = self.device_id
         logger.info(f"Sending message: {json.dumps(self.message_data)}")
         self.make_offloading_request(self.message_data)
 
-    @staticmethod
-    def make_offloading_request(message_data) -> None:
+    def make_offloading_request(self, message_data) -> None:
         try:
-            requests.post("http://localhost:8080/api/perform-offloading", json=message_data)
+            self.response = requests.post("http://localhost:8080/api/perform-offloading", json=message_data)
         except Exception as e:
             logger.error(f"Error: {e}")
 
@@ -31,8 +31,9 @@ if __name__ == "__main__":
         "start_layer_index": 0,
         "working_strategy": "working_strategy",
         "algorithm_version": "algorithm_version",
-        "payload": "payload_string"
+        "payload": {"message": "payload_string"}
     }
 
     device = SimpleDevice(device_id="test2", message_data=message_data_dict)
     device.send_message()
+    print(device.response.text)
