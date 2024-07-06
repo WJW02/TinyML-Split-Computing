70b6ab3e4c61abc679805352b24a1b25b8219217
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-06 13:45:26 +0200

    ADD: model builder

--- src/nn_model/model_builder.py
+++ src/nn_model/model_builder.py
@@ -1,30 +1,28 @@
 import os
 
 import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models
 
 from logger.Logger import Logger
+from model_data import ModelData
 
 logger = Logger().get_logger(__name__)
 
 
 class ModelBuilder:
-    def __init__(self, image_size: int = 10, num_classes: int = 2, image_paths: [] = list, labels: [] = list,
-                 dataset_path: str = '', save_path: str = ''):
-        self.image_size = image_size
+    def __init__(self, num_classes: int = 2, save_path: str = '', model_data: ModelData = None):
+        self.model_data = model_data
         self.num_classes = num_classes
-        self.image_paths = image_paths
-        self.labels = labels
-        self.dataset_path = dataset_path
         self.save_path = save_path
         self.model = self.build_model()
 
     def build_model(self):
         logger.info("Building the model")
         model = models.Sequential()
-        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(10, 10, 3), name="layer_0"))
+        model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                                input_shape=(self.model_data.image_size, self.model_data.image_size, 3),
+                                name="layer_0"))
         model.add(layers.MaxPooling2D((2, 2), name="layer_1"))
         model.add(layers.Flatten(name="layer_2"))
         model.add(layers.Dense(64, activation='relu', name="layer_3"))
@@ -34,16 +32,9 @@ class ModelBuilder:
 
     def prepare_data(self):
         logger.info("Preparing data for the model")
-        images = []
-        for path in self.image_paths:
-            img = tf.keras.preprocessing.image.load_img(
-                os.path.join(self.dataset_path, path),
-                target_size=(self.image_size, self.image_size)
-            )
-            img_array = tf.keras.preprocessing.image.img_to_array(img)
-            img_array /= 255.0
-            images.append(img_array)
-        x_train, x_test, y_train, y_test = train_test_split(images, self.labels, test_size=0.2, random_state=42)
+        images = [self.model_data.get_image_as_raw(path) for path in self.model_data.images_paths]
+        x_train, x_test, y_train, y_test = train_test_split(images, self.model_data.labels, test_size=0.2,
+                                                            random_state=42)
         return np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
 
     def train_model(self, epochs=10):
@@ -56,20 +47,24 @@ class ModelBuilder:
         loss, accuracy = self.model.evaluate(x_test, y_test)
         logger.info(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.4f}")
 
-    def predict(self, image_path: str):
-        if image_path is None:
-            logger.error("No data provided for prediction.")
-            return None
+    def predict(self, image_path: str, img_array):
         logger.info("Making a new prediction")
-        img = tf.keras.preprocessing.image.load_img(image_path, target_size=(self.image_size, self.image_size))
-        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        if image_path is not None:
+            logger.info("Image data will be loaded from file")
+            img_array = self.model_data.get_image_as_raw(image_path)
+        else:
+            logger.info("Image data passed as np array")
+
         img_array = np.expand_dims(img_array, axis=0)
-        img_array /= 255.0
+
+        if image_path is None:
+            img_array /= 255.0
+
         prediction = self.model.predict(img_array)
         return prediction
 
-    def save_model_as_h5(self, model_name: str = 'test_model'):
-        os.makedirs(os.path.dirname(f'models/{model_name}'), exist_ok=True)
-        model_path = f'{self.save_path}/{model_name}/{model_name}.h5'
+    def save_model(self, model_name: str = 'test_model'):
+        os.makedirs(os.path.dirname(f'{self.save_path}'), exist_ok=True)
+        model_path = f'{self.save_path}/{model_name}.keras'
         self.model.save(model_path)
         logger.info(f"Model saved at path: {model_path}")
