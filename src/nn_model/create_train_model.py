0dbcbf4e0ef36a7331c28a99b1a716ae062f4a84
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-06 13:46:26 +0200

    ADD: script to create and train a model and some data for training

--- src/nn_model/create_train_model.py
+++ src/nn_model/create_train_model.py
@@ -1,7 +1,7 @@
 import os
 
-# from model_builder import ModelBuilder
 from configs.configs import NeuralNetworkModelsConfigs
+from model_builder import ModelBuilder
 from model_data import ModelData
 
 # Configurations for model and dataset
@@ -9,7 +9,11 @@ model_name = NeuralNetworkModelsConfigs.DEFAULT_MODEL_NAME
 model_dir = NeuralNetworkModelsConfigs.MODELS_DIR_PATH
 image_size = NeuralNetworkModelsConfigs.IMAGE_SIZE
 dataset_path = NeuralNetworkModelsConfigs.DATASET_PATH
-num_samples = 5
+model_store_dir = NeuralNetworkModelsConfigs.MODELS_DIR_PATH
+num_samples = 100
+epochs = 10
+# Set a threshold for classification (you can adjust this based on your needs)
+threshold = NeuralNetworkModelsConfigs.BINARY_CLASSIFICATION_THRESHOLD
 
 # Ensure that the folders needed to store models data are created
 os.makedirs(f'{model_dir}/{model_name}/data', exist_ok=True)
@@ -22,3 +26,24 @@ model_data = ModelData(
     dataset_path=dataset_path
 )
 model_data.generate_dataset()
+
+# Build and train the model
+model = ModelBuilder(
+    num_classes=1,
+    save_path=model_store_dir,
+    model_data=model_data
+)
+model.train_model(epochs=epochs)
+model.evaluate_model()
+model.save_model(model_name=model_name)
+
+# Simple prediction to test the model
+correct_predictions = 0
+for data, label in zip(model_data.images_paths, model_data.labels):
+    image_path = data
+    prediction = model.predict(image_path=image_path, img_array=None)
+    # Check if the prediction is greater than the threshold
+    predicted_value = 1 if prediction[0][0] >= threshold else 0
+    if predicted_value == label[0]:
+        correct_predictions += 1
+print(f"Correct Predictions: {correct_predictions}/{len(model_data.images_paths)}")
