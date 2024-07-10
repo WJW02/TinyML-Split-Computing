791c8a10e8dec0a49e1c021d4e25458c029bef0f
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-10 20:56:09 +0200

    UPDATE: unified model manager

--- src/nn_model/offloading_model_manager.py
+++ src/nn_model/offloading_model_manager.py
@@ -1,7 +1,7 @@
 import time
 
+from custom_model import CustomModel
 from logger.Logger import Logger
-from tf_model_manager import TensorflowModelManager
 
 logger = Logger().get_logger(__name__)
 
@@ -16,9 +16,7 @@ class ModelManager:
         self.inference_time_device = []
         self.inference_time_edge = []
         self.model_analytics = {}
-        self.predictions = []
-        self.model_manager = TensorflowModelManager(model_name, model_path)
-        self.model_type = 'tf'
+        self.custom_model = CustomModel(model_name, model_path)
 
     def load_fake_model_information(self):
         self.num_layers = 5
@@ -27,29 +25,27 @@ class ModelManager:
         self.inference_time_edge = [0.05, 0.1, 0.15, 0.2, 0.25]  # Example inference times on edge
 
     def load_model_information(self):
-        return self.model_manager.num_layers
+        return self.custom_model.num_layers, self.model_analytics
 
-    def get_info(self):
+    def get_info_(self):
         return self.__dict__
 
-    def make_prediction(self, input_data, start_layer_index=0):
+    def trigger_prediction(self, input_data, start_layer_index=0):
         self.predictions = []
-        for layer_id in range(start_layer_index, self.model_manager.num_layers):
+        for layer_id in range(start_layer_index, self.custom_model.num_layers):
             t_begin = time.time()
             if layer_id == 0:
-                layer_output = self.model_manager.predict_single_layer(layer_id, input_data)
+                layer_output = self.custom_model.predict_single_layer(layer_id, input_data)
             else:
                 previous_layer_output = self.predictions[-1]
-
-                layer_output = self.model_manager.predict_single_layer(layer_id, previous_layer_output)
+                layer_output = self.custom_model.predict_single_layer(layer_id, previous_layer_output)
             self.predictions.append(layer_output)
             t_end = time.time()
-
             # Updates the analytics of the model
             self.evaluate_analytics(
                 layer_name=layer_id,
                 layer_inference_time=t_end - t_begin,
-                layer_size=self.model_manager.calculate_layer_size_in_bits(layer_output)
+                layer_size=self.custom_model.calculate_layer_size_in_bits(layer_output)
             )
 
     def evaluate_analytics(self, layer_name, layer_inference_time, layer_size):
