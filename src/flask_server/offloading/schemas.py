9c7f347a42757a896396a9dbf23bd9b99398cced
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-16 19:45:13 +0200

    UPDATE: new schemas

--- src/flask_server/offloading/schemas.py
+++ src/flask_server/offloading/schemas.py
@@ -1,13 +1,41 @@
 from marshmallow import Schema, fields
 
 
-class OffloadingSchema(Schema):
-    device_id = fields.String(required=True, metadata= {'example': 'device-1', 'type': 'string'})
+class OffloadingInformationSchema(Schema):
+    layers_inference_time = fields.Dict(
+        keys=fields.String,
+        values=fields.Float,
+        required=True,
+        metadata={
+            'example': {
+                'layer_0': 10.0,
+                'layer_1': 23.8,
+                'layer_2': 10.0,
+                'layer_3': 23.8,
+            }
+        }
+    )
+
+
+class OffloadingEvaluationSchema(Schema):
+    device_id = fields.String(required=True, metadata={'example': 'device-1', 'type': 'string'})
     model_name = fields.String(required=True)
-    start_layer_index = fields.Integer(required=False)
+    start_layer_index = fields.Integer(required=True)
+    offloading_information = fields.Nested(OffloadingInformationSchema, required=True)
     working_strategy = fields.String(required=False)
-    algorithm_version = fields.String(required=False)
+
+
+class OffloadingEvaluationMessages:
+    WRONG_KEY = "OPS: Wrong API key. Try again."
+    UNEXPECTED_ERROR = "ERROR: Unexpected error occurred!"
+    MISSING_KEYS = "OPS: Key missing. Try again."
+    SUCCESS_RESPONSE = "SUCCESS: Request processed successfully."
 
 
 class OffloadingErrorSchema(Schema):
     message = fields.String()
+
+
+class OffloadingModelInitializationSchema(Schema):
+    model_name = fields.String(required=True)
+    data = fields.String(required=True)
