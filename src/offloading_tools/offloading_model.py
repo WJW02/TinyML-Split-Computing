73ee96c4346d9937d01a6089d7a7db921ab766d4
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-17 16:09:21 +0200

    UPDATE: returns and logging

--- src/offloading_tools/offloading_model.py
+++ src/offloading_tools/offloading_model.py
@@ -1,6 +1,8 @@
 import json
 import time
 
+import pandas as pd
+
 from custom_models.custom_model import CustomModel
 from logger.Logger import Logger
 
@@ -9,7 +11,8 @@ logger = Logger().get_logger(__name__)
 
 class OffloadingModel:
 
-    def __init__(self, model_name, model_path, model_analytics_path, load_model_data: bool = False):
+    def __init__(self, model_name, model_path, model_analytics_path, load_model: bool = False,
+                 load_model_data: bool = False):
         self.model_name = model_name
         self.model_path = model_path
         self.model_analytics_path = model_analytics_path
@@ -21,12 +24,14 @@ class OffloadingModel:
         self.layers_inference_time = []
         self.model_analytics = {}
 
-        if load_model_data:
+        if load_model:
             self.load_custom_model(model_path)
+
+        if load_model_data:
             self.load_model_analytics(model_analytics_path)
 
     def get_info(self):
-        return self.to_dict(keep_model=False)
+        return self.to_dict(keep_model=False, filter_predictions=True)
 
     def load_custom_model(self, model_path: str = None):
         if model_path:
@@ -53,9 +58,10 @@ class OffloadingModel:
     def trigger_prediction(self, input_data, start_layer_index: int = 0):
         self.predictions = []
         layer = self.custom_model.get_model_layer(start_layer_index)
+        logger.info(f"Triggering prediction from layer: {layer}")
         expected_input_shape = layer.input.shape[1:]
-
         for layer_id, layer in enumerate(self.custom_model.model.layers):
+            logger.debug(f"Predicting layer: {layer_id}")
             start_time = time.time()
             prediction_data = input_data if layer_id == 0 else self.predictions[-1]
             prediction_data = self.custom_model.reshape_input_data(prediction_data, expected_input_shape)
@@ -82,20 +88,29 @@ class OffloadingModel:
         self.store_model_analytics()
 
     def store_model_analytics(self):
+        logger.info("Storing model analytics")
         # Updates the attributes related to layers inference times
         self.num_layers = self.custom_model.num_layers
         for layer in self.model_analytics.keys():
             self.layers_sizes.append(self.model_analytics[layer]['layer_size'])
             self.layers_inference_time.append(self.model_analytics[layer]['layer_inference_time'])
 
-    def to_dict(self, keep_model: bool = False):
+        logger.info(f"Model Analytics: {str(self.model_analytics)}")
+        model_analytics_df = pd.DataFrame.from_dict(self.model_analytics)
+        model_analytics_df.to_json(self.model_analytics_path)
+
+    def to_dict(self, keep_model: bool = False, filter_predictions: bool = False):
         offloading_model_as_dict = self.__dict__
         offloading_model_as_dict['custom_model'] = self.custom_model.__dict__
         # Remove the custom model from the dict
         if not keep_model:
             del offloading_model_as_dict['custom_model']['model']
+        if filter_predictions:
+            if self.predictions:
+                offloading_model_as_dict['predictions'] = offloading_model_as_dict['predictions'][-1]
         return offloading_model_as_dict
 
     def perform_model_initialization(self, input_data):
         logger.info("Performing Model Initialization")
         self.trigger_prediction(input_data=input_data, start_layer_index=0)
+        return self.get_info()
