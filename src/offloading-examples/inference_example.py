3db480e595a664a9daa49a3384830a59584ab2f1
Fabio Bove <fabio.bove.dr@gmail.com>
2024-07-18 16:34:20 +0200

    UPDATE: removed useless code

---
+++ src/offloading-examples/inference_example.py
@@ -0,0 +1,53 @@
+import base64
+import os
+
+import requests
+import tensorflow as tf
+
+from configs.configs import CustomModelExample
+from custom_models.custom_model import CustomModel
+from custom_models.model_data import ModelData
+from offloading_tools.offloading_model import OffloadingModel
+
+if __name__ == '__main__':
+    # Configurations for model and dataset
+    model_name = "test_model.keras"
+    model_path = "../custom_models/models/test_model/test_model.keras"
+    image_size = CustomModelExample.IMAGE_SIZE
+
+    model_data = ModelData()
+    dataset_path = '../custom_models/models/test_model/data'
+    model_data.load_from_path(path=dataset_path)
+    image_path, label = (model_data.images_paths[0], model_data.labels[0])
+    image_path = os.path.join(dataset_path, image_path)
+    image_array = model_data.get_image_as_raw(image_path, expand_dims=True)
+
+    # Build and train the model
+    custom_model = CustomModel(model_data=model_data)
+    custom_model.load_model(model_path=model_path)
+
+    offloading_model = OffloadingModel(
+        model_name=model_name,
+        model_path=model_path,
+        model_analytics_path="../custom_models/models/test_model/analytics.json",
+        load_model=True,
+        load_model_data=True,
+    )
+    offloading_model.trigger_prediction(input_data=image_array, start_layer_index=0, end_layer_index=2)
+    last_prediction = offloading_model.predictions[-1]
+
+    intermediate_data = last_prediction
+    print(type(intermediate_data))
+    original_shape = intermediate_data.shape
+    intermediate_data_b64 = base64.b64encode(intermediate_data.tobytes()).decode('utf-8')
+
+    # Sending example image to the server
+    url = 'http://localhost:8080/api/offloading/model-inference'
+    params = {
+        'model_name': 'test_model',
+        'model_data': intermediate_data_b64,
+        'shape': original_shape,
+        'start_layer_index': 2,
+    }
+    response = requests.post(url, json=params)
+    print(response.text)
--- src/offloading-examples/initialization_example.py
+++ src/offloading-examples/initialization_example.py
@@ -1,17 +1,10 @@
 import base64
 import os
 
-import numpy as np
 import requests
-from PIL import Image
+import tensorflow as tf
 
 from custom_models.model_data import ModelData
-import requests
-import json
-import os
-import numpy as np
-import base64
-import tensorflow as tf
 
 if __name__ == '__main__':
     # Loading example data
@@ -22,28 +15,17 @@ if __name__ == '__main__':
     image_path = os.path.join(dataset_path, image_path)
     image_array = model_data.get_image_as_raw(image_path, expand_dims=True)
 
-    # Convert the image_array to TensorFlow EagerTensor
-    image_array_tf = tf.convert_to_tensor(image_array)
-
     # Convert the TensorFlow EagerTensor to a NumPy array
-    image_array_np = image_array_tf.numpy()
-
-    # Resize or crop the image to match (20, 20)
-    resized_image = tf.image.resize(image_array_np, [20, 20])  # Resize using TensorFlow
-
-    # Convert the resized image to NumPy array and normalize if necessary
-    image_array_resized = resized_image.numpy()
-    image_array_resized = image_array_resized / 255.0  # Normalize to [0, 1] if required
-
+    image_array_np = image_array.numpy()
     # Convert the image_array (tensor) to a base64-encoded string
-    image_array_b64 = base64.b64encode(image_array_resized.tobytes()).decode('utf-8')
+    image_array_b64 = base64.b64encode(image_array_np.tobytes()).decode('utf-8')
 
     # Sending example image to the server
     url = 'http://localhost:8080/api/offloading/model-initialization'
     params = {
         'model_name': 'test_model',
         'model_data': image_array_b64,
-        'shape': image_array_resized.shape  # Send the shape to reconstruct the tensor on the server
+        'shape': image_array_np.shape  # Send the shape to reconstruct the tensor on the server
     }
 
     response = requests.post(url, json=params)
